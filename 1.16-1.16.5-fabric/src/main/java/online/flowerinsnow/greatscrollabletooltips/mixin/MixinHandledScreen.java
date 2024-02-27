7b7b5ae0b91f126809caab0a5e0c8736f2208d77
flowerinsnow <flowerimsnow@hotmail.com>
2024-02-28 07:09:21 +0800

    7.x for 1.16.2-1.16.5-fabric

---
+++ 1.16-1.16.5-fabric/src/main/java/online/flowerinsnow/greatscrollabletooltips/mixin/MixinHandledScreen.java
@@ -0,0 +1,44 @@
+package online.flowerinsnow.greatscrollabletooltips.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import online.flowerinsnow.greatscrollabletooltips.event.RenderMouseoverTooltipEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HandledScreen.class)
+@Environment(EnvType.CLIENT)
+public class MixinHandledScreen<T extends ScreenHandler> extends Screen {
+    @Unique
+    @SuppressWarnings("unchecked")
+    private final HandledScreen<T> THIS = (HandledScreen<T>) (Object) this;
+
+    @Shadow
+    protected Slot focusedSlot;
+
+    protected MixinHandledScreen() {
+        super(null);
+    }
+
+    @Inject(
+            method = "drawMouseoverTooltip",
+            at = @At("RETURN")
+    )
+    public void drawMouseoverTooltip(MatrixStack matrices, int x, int y, CallbackInfo ci) {
+        //noinspection DataFlowIssue
+        if (this.client.player.inventory.getCursorStack().isEmpty() && this.focusedSlot != null && this.focusedSlot.hasStack()) {
+            RenderMouseoverTooltipEvent.Post.EVENT.invoker().startDrawMouseoverTooltip(THIS, matrices, this.focusedSlot.getStack(), x, y);
+        } else {
+            RenderMouseoverTooltipEvent.Miss.EVENT.invoker().onMiss(THIS);
+        }
+    }
+}
