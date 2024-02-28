2c88c13e3679a37724fedc9e7334670f4cd21435
flowerinsnow <flowerimsnow@hotmail.com>
2024-02-29 03:36:46 +0800

    Update properties

--- 1.8.9-1.10-forge/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
+++ 1.8.9-1.10-forge/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
@@ -26,7 +26,7 @@ import java.io.File;
 public class GreatScrollableTooltips {
     public static final String MODID = "great-scrollable-tooltips";
     public static final String NAME = "Great Scrollable Tooltips";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.2";
     private static GreatScrollableTooltips instance;
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
