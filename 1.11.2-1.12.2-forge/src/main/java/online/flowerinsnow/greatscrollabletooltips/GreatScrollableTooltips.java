f15b24e4f43f4d861cca0b8a0332f5674de90350
flowerinsnow <flowerimsnow@hotmail.com>
2024-03-04 20:31:32 +0800

    Update version number

--- 1.11.2-1.12.2-forge/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
+++ 1.11.2-1.12.2-forge/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
@@ -26,7 +26,7 @@ import java.io.File;
 public class GreatScrollableTooltips {
     public static final String MODID = "great-scrollable-tooltips";
     public static final String NAME = "Great Scrollable Tooltips";
-    public static final String VERSION = "4.1.1";
+    public static final String VERSION = "4.1.2";
 
     private static GreatScrollableTooltips instance;
 
