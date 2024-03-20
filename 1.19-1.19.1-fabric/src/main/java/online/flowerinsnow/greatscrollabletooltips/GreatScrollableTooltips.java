e9788a5162b20dd56dad8a1a77a1e84467e8e7be
flowerinsnow <flowerimsnow@hotmail.com>
2024-03-20 18:06:11 +0800

    Allow scroll horizontally in version 11
    Allow scroll with key binding in version 11
    
    #26
    
    #9

--- 1.19-1.19.1-fabric/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
+++ 1.19-1.19.1-fabric/src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
@@ -4,11 +4,17 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.util.ActionResult;
 import online.flowerinsnow.greatscrollabletooltips.config.Config;
+import online.flowerinsnow.greatscrollabletooltips.event.HandledScreenKeyPressedEvent;
 import online.flowerinsnow.greatscrollabletooltips.event.MouseScrolledInParentElementEvent;
 import online.flowerinsnow.greatscrollabletooltips.event.RenderMouseoverTooltipEvent;
+import online.flowerinsnow.greatscrollabletooltips.listener.CursorKeyListener;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,8 +27,14 @@ public class GreatScrollableTooltips implements ClientModInitializer {
 
 	private Config config;
 
+	private int horizontal;
 	private int vertical;
 	private boolean rendering;
+
+	public static final KeyBinding KEY_BINDING_SCROLL_UP = new KeyBinding("great-scrollable-tooltips.key-binding.scroll-up", GLFW.GLFW_KEY_UP, "great-scrollable-tooltips.key-binding.category");
+	public static final KeyBinding KEY_BINDING_SCROLL_LEFT = new KeyBinding("great-scrollable-tooltips.key-binding.scroll-left", GLFW.GLFW_KEY_LEFT, "great-scrollable-tooltips.key-binding.category");
+	public static final KeyBinding KEY_BINDING_SCROLL_DOWN = new KeyBinding("great-scrollable-tooltips.key-binding.scroll-down", GLFW.GLFW_KEY_DOWN, "great-scrollable-tooltips.key-binding.category");
+	public static final KeyBinding KEY_BINDING_SCROLL_RIGHT = new KeyBinding("great-scrollable-tooltips.key-binding.scroll-right", GLFW.GLFW_KEY_RIGHT, "great-scrollable-tooltips.key-binding.category");
 
 	@Override
 	public void onInitializeClient() {
@@ -35,7 +47,11 @@ public class GreatScrollableTooltips implements ClientModInitializer {
 		MouseScrolledInParentElementEvent.EVENT.register((parentElement, mouseX, mouseY, amount) -> {
 			MinecraftClient client = MinecraftClient.getInstance();
 			if (client.currentScreen != null && config.enable && rendering) {
-				vertical += (int) amount;
+				if (Screen.hasShiftDown()) {
+					horizontal += (int) amount;
+				} else {
+					vertical += (int) amount;
+				}
 			}
 			return ActionResult.PASS;
 		});
@@ -52,9 +68,17 @@ public class GreatScrollableTooltips implements ClientModInitializer {
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.currentScreen == null) {
+				horizontal = 0;
 				vertical = 0;
 			}
 		});
+
+		HandledScreenKeyPressedEvent.EVENT.register(new CursorKeyListener());
+
+		KeyBindingHelper.registerKeyBinding(KEY_BINDING_SCROLL_UP);
+		KeyBindingHelper.registerKeyBinding(KEY_BINDING_SCROLL_LEFT);
+		KeyBindingHelper.registerKeyBinding(KEY_BINDING_SCROLL_DOWN);
+		KeyBindingHelper.registerKeyBinding(KEY_BINDING_SCROLL_RIGHT);
 	}
 
 	public static GreatScrollableTooltips getInstance() {
@@ -65,11 +89,23 @@ public class GreatScrollableTooltips implements ClientModInitializer {
 		return config;
 	}
 
+	public int getHorizontal() {
+		return horizontal;
+	}
+
+	public void setHorizontal(int horizontal) {
+		this.horizontal = horizontal;
+	}
+
 	public int getVertical() {
 		return vertical;
 	}
 
 	public void setVertical(int vertical) {
 		this.vertical = vertical;
+	}
+
+	public boolean isRendering() {
+		return rendering;
 	}
 }
